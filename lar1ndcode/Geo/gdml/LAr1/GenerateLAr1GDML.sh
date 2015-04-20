81866a4540c508a3f38e6ec431f4f0eed8689881
gamez <gamez@hepgpu1.hep.manchester.ac.uk>
2015-04-20 14:17:31 +0100

    rename output file

--- lar1ndcode/Geo/gdml/LAr1/GenerateLAr1GDML.sh
+++ lar1ndcode/Geo/gdml/LAr1/GenerateLAr1GDML.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #Generate geometry without wires -- t is tpb percent coverage; b corresponds to scintillation bars
-./generate_gdml.pl -w 0 -t 1 -b 0 -i lar1-gdml-parameters.xml -o lar1-gdml-fragments.xml
-./make_gdml.pl -i lar1-gdml-fragments.xml -o lar1nd_nowires.gdml
+./generate_gdml_refl.pl -w 0 -t 0 -b 0 -i lar1-gdml-parameters.xml -o lar1-gdml-fragments.xml
+./make_gdml.pl -i lar1-gdml-fragments.xml -o lar1nd_refl_nowires.gdml
 
 #Generate geometry with wires
-./generate_gdml.pl -w 1 -t 1 -b 0 -i lar1-gdml-parameters.xml -o lar1-gdml-fragments.xml
-./make_gdml.pl -i lar1-gdml-fragments.xml -o lar1nd.gdml
+./generate_gdml_refl.pl -w 1 -t 0 -b 0 -i lar1-gdml-parameters.xml -o lar1-gdml-fragments.xml
+./make_gdml.pl -i lar1-gdml-fragments.xml -o lar1nd_refl.gdml
 
 #Copy both back up one level to be seen by LArSoft jobs
-cp lar1nd_nowires.gdml ..
-cp lar1nd.gdml ..
+cp lar1nd_refl_nowires.gdml ..
+cp lar1nd_refl.gdml ..

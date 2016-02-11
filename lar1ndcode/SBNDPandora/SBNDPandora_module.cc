aeb11efa5ec0e553097b33f23bc181642ccd0133
John Marshall <marshall@hep.phy.cam.ac.uk>
2016-02-11 12:23:09 +0000

    Update SBNDPandora to reflect changes to LArPandora interface class and make use of MultiPandoraApi.

--- lar1ndcode/SBNDPandora/SBNDPandora_module.cc
+++ lar1ndcode/SBNDPandora/SBNDPandora_module.cc
@@ -5,16 +5,16 @@
  *
  */
 
-// Framework Includes
 #include "art/Framework/Core/ModuleMacros.h"
 
-// Local includes
-#include "LArPandoraInterface/LArPandoraParticleCreator.h"
+#include "Geometry/Geometry.h"
 
-// std includes
+#include "LArStitching/MultiPandoraApi.h"
+
+#include "LArPandoraInterface/LArPandora.h"
+
+#include <set>
 #include <string>
-
-//------------------------------------------------------------------------------------------------------------------------------------------
 
 namespace lar_pandora
 {
@@ -22,29 +22,40 @@ namespace lar_pandora
 /**
  *  @brief  SBNDPandora class
  */
-class SBNDPandora : public LArPandoraParticleCreator
+class SBNDPandora : public LArPandora
 {
 public: 
-
     /**
      *  @brief  Constructor
      *
-     *  @param  pset
+     *  @param  pset the parameter set
      */
     SBNDPandora(fhicl::ParameterSet const &pset);
 
-    /**
-     *  @brief  Destructor
-     */
-    virtual ~SBNDPandora();
+    int GetVolumeIdNumber(const unsigned int cryostat, const unsigned int tpc) const;
 
 private:
+    void CreatePandoraInstances();
 
-    unsigned int GetPandoraVolumeID(const unsigned int cstat, const unsigned int tpc) const;
-    void ConfigurePandoraGeometry() const;
+    /**
+     *  @brief  Create primary pandora instance
+     *
+     *  @param  stitchingConfigFileName the pandora settings stitching config file name
+     */
+    void CreatePrimaryPandoraInstance(const std::string &stitchingConfigFileName);
 
-    bool            m_useLeftVolume;      ///<
-    bool            m_useRightVolume;       ///<
+    /**
+     *  @brief  Create daughter pandora instances
+     *
+     *  @param  theGeometry the geometry handle
+     *  @param  configFileName the pandora settings config file name
+     */
+    void CreateDaughterPandoraInstances(const art::ServiceHandle<geo::Geometry> &theGeometry, const std::string &configFileName);
+
+    typedef std::set<int> IntSet;   ///<
+
+    bool    m_useLeftVolume;        ///<
+    bool    m_useRightVolume;       ///<
 };
 
 DEFINE_ART_MODULE(SBNDPandora)
@@ -54,81 +65,123 @@ DEFINE_ART_MODULE(SBNDPandora)
 //------------------------------------------------------------------------------------------------------------------------------------------
 // implementation follows
 
-// LArSoft includes
-#include "Geometry/Geometry.h"
+#include "cetlib/exception.h"
 
-// Local includes (LArContent) 
+#include "Api/PandoraApi.h"
+
 #include "LArContent.h"
 
-// Local includes (LArPandora)
 #include "lar1ndcode/SBNDPandora/SBNDPseudoLayerPlugin.h"
 #include "lar1ndcode/SBNDPandora/SBNDTransformationPlugin.h"
-#include "lar1ndcode/SBNDPandora/SBNDGeometryHelper.h"
 
-namespace lar_pandora {
-
-SBNDPandora::SBNDPandora(fhicl::ParameterSet const &pset) : LArPandoraParticleCreator(pset)
+namespace lar_pandora
 {
-    m_useLeftVolume = pset.get<bool>("UseLeftVolume",true);
-    m_useRightVolume = pset.get<bool>("UseRightVolume",true);
+
+SBNDPandora::SBNDPandora(fhicl::ParameterSet const &pset) :
+    LArPandora(pset)
+{
+    m_useLeftVolume = pset.get<bool>("UseLeftVolume", true);
+    m_useRightVolume = pset.get<bool>("UseRightVolume", true);
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-SBNDPandora::~SBNDPandora()
+int SBNDPandora::GetVolumeIdNumber(const unsigned int cryostat, const unsigned int tpc) const
 {
-}
-
-//------------------------------------------------------------------------------------------------------------------------------------------
-
-void SBNDPandora::ConfigurePandoraGeometry() const
-{
-    mf::LogDebug("LArPandora") << " *** SBNDPandora::ConfigurePandoraGeometry(...) *** " << std::endl;
-
-    // Identify the Geometry and load the plugins
     art::ServiceHandle<geo::Geometry> theGeometry;
+    const geo::TPCGeo &theTpcGeo(theGeometry->TPC(tpc, cryostat));
 
+    // Left drift volume: negative drift direction
+    if (theTpcGeo.DriftDirection() == geo::kNegX)
+    {
+        if (m_useLeftVolume)
+            return 0;
+    }
+
+    // Right drift volume: positive drift direction
+    if (theTpcGeo.DriftDirection() == geo::kPosX)
+    {
+        if (m_useRightVolume)
+            return 1;
+    }
+
+    throw pandora::StatusCodeException(pandora::STATUS_CODE_INVALID_PARAMETER);
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void SBNDPandora::CreatePandoraInstances()
+{
+    mf::LogDebug("LArPandora") << " *** SBNDPandora::CreatePandoraInstances(...) *** " << std::endl;
+
+    art::ServiceHandle<geo::Geometry> theGeometry;
     if (std::string::npos == theGeometry->DetectorName().find("lar1nd"))
     {
         mf::LogError("LArPandora") << " Error! Using invalid geometry: " << theGeometry->DetectorName() << std::endl;
         throw cet::exception("LArPandora") << " SBNDPandora::ConfigurePandoraGeometry --- Invalid Geometry: " << theGeometry->DetectorName();
     }
 
-    for (PandoraInstanceMap::const_iterator pIter = m_pandoraInstanceMap.begin(), pIterEnd = m_pandoraInstanceMap.end(); 
-        pIter != pIterEnd; ++pIter)
+    cet::search_path sp("FW_SEARCH_PATH");
+    std::string stitchingConfigFileName, configFileName;
+    if (!sp.find_file(m_stitchingConfigFile, stitchingConfigFileName) || !sp.find_file(m_configFile, configFileName))
     {
-        const unsigned int      volumeID = pIter->first;
-        const pandora::Pandora *pPandora = pIter->second;
-
-        const bool isForward((0 == volumeID) ? true : false); // ATTN: Sign of rotation matrix is taken from Volume ID
-    
-        PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, LArContent::SetLArPseudoLayerPlugin(*pPandora, 
-            new SBNDPseudoLayerPlugin));
-
-        PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, LArContent::SetLArTransformationPlugin(*pPandora, 
-            new SBNDTransformationPlugin(isForward)));
+        mf::LogError("LArPandora") << "   Failed to find one of: " << m_stitchingConfigFile << ", " << m_configFile << std::endl;
+        throw pandora::StatusCodeException(pandora::STATUS_CODE_NOT_FOUND);
     }
+
+    this->CreatePrimaryPandoraInstance(stitchingConfigFileName);
+    this->CreateDaughterPandoraInstances(theGeometry, configFileName);
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-unsigned int SBNDPandora::GetPandoraVolumeID(const unsigned int cstat, const unsigned int tpc) const
-{    
-    const SBNDGeometryHelper::SBNDVolume volumeID(SBNDGeometryHelper::GetVolumeID(cstat, tpc));
+void SBNDPandora::CreatePrimaryPandoraInstance(const std::string &stitchingConfigFileName)
+{
+    m_pPrimaryPandora = this->CreateNewPandora();
+    MultiPandoraApi::AddPrimaryPandoraInstance(m_pPrimaryPandora);
+    PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, PandoraApi::ReadSettings(*m_pPrimaryPandora, stitchingConfigFileName));
+    PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, LArContent::SetLArPseudoLayerPlugin(*m_pPrimaryPandora, new lar_pandora::SBNDPseudoLayerPlugin));
+}
 
-    if (SBNDGeometryHelper::kLeftVolume == volumeID) 
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void SBNDPandora::CreateDaughterPandoraInstances(const art::ServiceHandle<geo::Geometry> &theGeometry, const std::string &configFileName)
+{
+    if (!m_pPrimaryPandora)
+        throw pandora::StatusCodeException(pandora::STATUS_CODE_NOT_INITIALIZED);
+
+    IntSet volumeIdNumbers;
+
+    for (unsigned int icstat = 0; icstat < theGeometry->Ncryostats(); ++icstat)
     {
-        if (m_useLeftVolume) 
-            return 0;
-    }
+        for (unsigned int itpc = 0; itpc < theGeometry->NTPC(icstat); ++itpc)
+        {
+            try
+            {
+                const int volumeIdNumber(this->GetVolumeIdNumber(icstat, itpc));
 
-    if (SBNDGeometryHelper::kRightVolume == volumeID) 
-    {
-        if (m_useRightVolume) 
-            return 1;
-    }
+                if (!volumeIdNumbers.insert(volumeIdNumber).second)
+                    continue;
 
-    throw pandora::StatusCodeException(pandora::STATUS_CODE_INVALID_PARAMETER);
+                const bool isForward(0 == volumeIdNumber); // ATTN: Sign of rotation matrix is taken from Volume ID
+                const bool isPositiveDrift(1 == volumeIdNumber);
+
+                std::ostringstream volumeIdString("lar1nd_");
+                volumeIdString << volumeIdNumber;
+
+                const pandora::Pandora *const pPandora = this->CreateNewPandora();
+                MultiPandoraApi::AddDaughterPandoraInstance(m_pPrimaryPandora, pPandora);
+                MultiPandoraApi::SetVolumeInfo(pPandora, new VolumeInfo(volumeIdNumber, volumeIdString.str(), pandora::CartesianVector(0.f, 0.f, 0.f), isPositiveDrift));
+                PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, LArContent::SetLArPseudoLayerPlugin(*pPandora, new lar_pandora::SBNDPseudoLayerPlugin));
+                PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, LArContent::SetLArTransformationPlugin(*pPandora, new lar_pandora::SBNDTransformationPlugin(isForward)));
+                PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, PandoraApi::ReadSettings(*pPandora, configFileName));
+            }
+            catch (pandora::StatusCodeException &)
+            {
+                mf::LogDebug("SBNDPandora") << "    No volume ID for this TPC..." << std::endl;
+            }
+        }
+    }
 }
 
 } // namespace lar_pandora

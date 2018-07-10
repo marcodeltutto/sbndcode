a1082987383e65249e5223650f123bee0ef8b4ee
Sbnd <sbnddqm@lariat-daq04.fnal.gov>
2018-07-10 11:06:34 -0500

    add in good header service

---
+++ sbndcode/VSTAnalysis/GoodHeader_service.cc
@@ -0,0 +1,9 @@
+#include "art/Framework/Services/Registry/ActivityRegistry.h"
+#include "art/Framework/Services/Registry/ServiceMacros.h"
+#include "fhiclcpp/ParameterSet.h"
+
+#include "GoodHeader.hh"
+
+
+DEFINE_ART_SERVICE(daqAnalysis::GoodHeader)
+

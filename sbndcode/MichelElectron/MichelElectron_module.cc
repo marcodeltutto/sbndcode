e6a91ce409d22a91b662c933ebd89a34706fbc3f
Julia Tena Vidal <jtenavid@fnal.gov>
2019-09-19 14:52:34 -0500

    accessing OpWaveform data member

--- sbndcode/MichelElectron/MichelElectron_module.cc
+++ sbndcode/MichelElectron/MichelElectron_module.cc
@@ -17,11 +17,16 @@
 #include "fhiclcpp/ParameterSet.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
 
-
 #include "larcore/Geometry/Geometry.h"
 #include "lardataobj/RawData/OpDetWaveform.h"
 #include "lardataobj/RecoBase/OpHit.h"
 #include "lardata/DetectorInfoServices/DetectorClocksService.h"
+
+#include "TTree.h"
+#include "TFile.h"
+#include "TNtuple.h"
+#include "TROOT.h"
+
 
 class MichelElectron;
 
@@ -54,6 +59,15 @@ private:
    ******************************************/
   std::string TruthLabel, G4Label, ParticleLabel, HitFinderLabel, RecoTrackLabel, RecoShowerLabel, RecoPIDLabel, RecoCaloLabel, photonLabel ; 
 
+  /******************************************
+   *  TREES                                 *
+   ******************************************/
+  TTree * opt_tree ;
+
+  /******************************************
+   *  DATA MEMBERS                          *
+   ******************************************/
+
   int event_id;
 };
 
@@ -64,7 +78,6 @@ MichelElectron::MichelElectron(fhicl::ParameterSet const& p)
 {
   // Call appropriate consumes<>() for any products to be retrieved by this module.
   TruthLabel = p.get<std::string>("TruthLabel");
-  G4Label = p.get<std::string>("G4Label");
   ParticleLabel = p.get<std::string>("PFParticleModule","pandora");
   HitFinderLabel = p.get<std::string>("HitFinderModule","linecluster");
   RecoTrackLabel = p.get<std::string>("RecoTrackLabel","pandoraTrack");
@@ -72,7 +85,7 @@ MichelElectron::MichelElectron(fhicl::ParameterSet const& p)
   RecoCaloLabel = p.get<std::string>("RecoCaloLabel","pandoraCalo");
   RecoPIDLabel = p.get<std::string>("RecoPIDLabel","pandoraPid");
 
-  photonLabel = p.get<std::string>("photonLabel","g4Label");
+  photonLabel = p.get<std::string>("photonLabel","optdaq");
   
   this->reconfigure(p);
 }
@@ -84,11 +97,17 @@ void MichelElectron::analyze(art::Event const& e)
   event_id = e.id().event();
 
   if( !e.isRealData()){
+    // grab a data productfrom the event
+    auto const& wvFormsHandle = *e.getValidHandle<std::vector<raw::OpDetWaveform>>("opdaq");
+    //    art::Handle<std::vector<raw::OpDetWaveform>> wvFormsHandle;
+    //e.getByLabel(photonLabel, wvFormsHandle ) ;
+    std::cout << " is real data after handle " << std::endl;
 
-    art::Handle<std::vector<raw::OpDetWaveform>> wvFormsHandle;
-
-    e.getByLabel(photonLabel, wvFormsHandle ) ;
-
+    for( unsigned int i = 0 ; i < wvFormsHandle.size(); ++i ) {
+      std::cout << " IN LOOP " << std::endl;
+      auto const& wforms = wvFormsHandle.at(i);
+      std::cout << wforms.TimeStamp() << std::endl;
+    }
   }
 }
 
@@ -97,11 +116,25 @@ void MichelElectron::beginJob()
   // Implementation of optional member function here.
 
   event_id = -999;
+  
+  // Define output tree structure here
+  opt_tree   = new TTree( "opt_tree",    "Opt tree: General information about the light information in the event" ) ;
+  
+  opt_tree -> Branch( "event_id",     & event_id,     "event_id/I" ) ;
+
+  opt_tree -> SetDirectory(0);
 }
 
 void MichelElectron::endJob()
 {
   // Implementation of optional member function here.
+  // store tree
+  TFile f("output_wftree.root", "RECREATE");
+  opt_tree -> Write() ;
+  f.Write();
+  f.Close();
+
+  event_id = -999;
 }
 
 void MichelElectron::reconfigure(fhicl::ParameterSet const & p)
